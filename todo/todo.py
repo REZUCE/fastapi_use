54f3aac40a3657cf3471bc193139256b55df755d
Vladimir Lisicyn <>
2024-08-07 18:39:33 +0300

    add Path and Query parameters

--- todo/todo.py
+++ todo/todo.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Path
 from todo.schema import TodoSchema
 
 todo_router = APIRouter()
@@ -8,10 +8,6 @@ todo_list = []
 
 @todo_router.post("/todo")
 async def add_todo(todo: TodoSchema) -> dict:
-    # Если засунуть объект todo в список, то получится словарь, потому что у объекта есть
-    # ключ и значение если его принтануть или сделать так todo.id.
-    print(todo)
-    print(todo.id)
     todo_list.append(todo)
     return {"message": "Todo added successfully"}
 
@@ -19,3 +15,37 @@ async def add_todo(todo: TodoSchema) -> dict:
 @todo_router.get("/todo")
 async def retrieve_todos() -> dict:
     return {"todo": todo_list}
+
+
+"""
+Здесь добавлен параметр пути. Path помогает отличать параметры пути от других аргументов
+присутствующих в функции маршрута.
+
+Класс Path принимает первый позиционный аргумент, равный None или
+многоточие (...). Если в качестве первого аргумента задано многоточие (...),
+параметр пути становится обязательным. Класс Path также содержит
+аргументы, используемые для числовой проверки, если параметр пути
+является числом. Определения включают gt и le – gt означает больше, а le
+означает меньше. При использовании маршрут будет проверять параметр пути
+на соответствие этим аргументам.
+
+Параметр запроса — это необязательный параметр, который обычно появляется
+после вопросительного знака в URL-адресе. Он используется для фильтрации
+запросов и возврата определенных данных на основе предоставленных запросов.
+В функции обработчика маршрута аргумент, не совпадающий с параметром пути,
+является запросом. Вы также можете определить запрос, создав экземпляр класса
+FastAPI Query() в аргументе функции, например:
+async query_route(query: str = Query(None):
+    return query
+
+"""
+@todo_router.get("/todo/{todo_id}")
+async def get_single_todo(todo_id: int = Path(..., description="The ID of the todo to retrieve.")) -> dict:
+    for todo in todo_list:
+        if todo.id == todo_id:
+            return {
+                "todo": todo
+            }
+    return {
+        "message": "Todo with supplied ID doesn't exist."
+    }

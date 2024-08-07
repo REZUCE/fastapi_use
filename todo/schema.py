425d6c2e45e0c0a36bc61b3259e08aa9fceb1075
Vladimir Lisicyn <>
2024-08-07 18:49:30 +0300

    hacan add ыыÐÑsuch an Example Schema

--- todo/schema.py
+++ todo/schema.py
@@ -18,3 +18,12 @@ class TodoSchema(BaseModel):
     """
     id: int
     item: str
+
+    # Можно добавить вот такой пример request body.
+    class Config:
+        Schema_extra = {
+            "Example": {
+                "id": 1,
+                "item": "Example schema!"
+            }
+        }
